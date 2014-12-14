c3a7719e12f1339dbb1694bbe422339cff77429a
Pedro Algarvio <pedro@algarvio.me>
2014-12-14 23:08:21 +0000

    Wait for any running `salt-call state.sls *' processes

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -1,4 +1,3 @@
-# Random 1-10 seconds sleep in order not to call state.sls at the same time
-sleep $(($RANDOM % 10))
-
+# Wait for a running salt-call state.sls
+wait $(pgrep -f 'salt-call state.sls')
 salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}}" concurrent=true
