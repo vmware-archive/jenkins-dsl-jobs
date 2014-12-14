a8024d18b042aff7855f350fa4f1d9e8c1ea6bec
Pedro Algarvio <pedro@algarvio.me>
2014-12-14 22:57:12 +0000

    Activate the virtualenv

--- scripts/set-commit-status.sh
+++ scripts/set-commit-status.sh
@@ -1,1 +1,2 @@
+[ "${VIRTUALENV_NAME}" != "" ] && . /srv/virtualenvs/${VIRTUALENV_NAME}/bin/activate > /dev/null 2>&1
 github-commit-status --auth-token=${GITHUB_AUTH_TOKEN} --repo=${GITHUB_REPO} --context=${COMMIT_STATUS_CONTEXT} --target-url=${BUILD_URL} ${GIT_COMMIT}
