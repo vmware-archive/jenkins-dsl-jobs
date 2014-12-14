e4bb062bc5a012ecf482f00419c5a694765d609a
Pedro Algarvio <pedro@algarvio.me>
2014-12-14 23:19:31 +0000

    Posix echo support. Syntax fix.

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -1,11 +1,14 @@
+#!/bin/sh
+
 # Wait for a running salt-call state.sls
-running_salt_call=$(pgrep -f 'salt-call state.sls')
-if [ "${running_salt_call}" != ""]; then
-    echo -n "Waiting on running salt-call to finish "
-    while [ -e /proc/${running_salt_call} ]; do
+running_salt_call="$(pgrep -f 'salt-call state.sls')"
+if [ "${running_salt_call}" != "" ]; then
+    printf "%s" "Waiting on running salt-call to finish "
+    while [ -e "/proc/${running_salt_call}" ]; do
         sleep 1
-        echo -n "."
+        printf "%s" "."
     done
-    echo "Done!"
+    printf "%s" "Done!\n"
 fi
+
 salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}}"
