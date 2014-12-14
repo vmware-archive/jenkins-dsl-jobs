a7959eb96b3681cbec9fa6de3f11c662f045f380
Pedro Algarvio <pedro@algarvio.me>
2014-12-14 23:15:01 +0000

    We can't `wait` on processes not started on the same shell

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -1,3 +1,11 @@
 # Wait for a running salt-call state.sls
-wait $(pgrep -f 'salt-call state.sls')
-salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}}" concurrent=true
+running_salt_call=$(pgrep -f 'salt-call state.sls')
+if [ "${running_salt_call}" != ""]; then
+    echo -n "Waiting on running salt-call to finish "
+    while [ -e /proc/${running_salt_call} ]; do
+        sleep 1
+        echo -n "."
+    done
+    echo "Done!"
+fi
+salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}}"
