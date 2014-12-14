22d547a4daa86c38e62d5295c57b26ca73095e0e
Pedro Algarvio <pedro@algarvio.me>
2014-12-14 22:34:09 +0000

    Try not to call state.sls at the same time

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -1,1 +1,4 @@
+# Random 1-10 seconds sleep in order not to call state.sls at the same time
+sleep $(($RANDOM % 10))
+
 salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}}"
