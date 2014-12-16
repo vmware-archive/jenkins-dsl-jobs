0fc138dc5d19becf2cdc6a58ee76f3f43a523fb2
Pedro Algarvio <pedro@algarvio.me>
2014-12-16 01:41:20 +0000

    Get a reader

--- libnacl/jobs.groovy
+++ libnacl/jobs.groovy
@@ -3,7 +3,7 @@
 // Common variable Definitions
 def github_repo = 'saltstack/libnacl'
 def repo_api = new URL("https://api.github.com/${github_repo}")
-def repo_data = new groovy.json.JsonSlurper().parse(repo_api)
+def repo_data = new groovy.json.JsonSlurper().parse(repo_api.newReader())
 def project_description = repo_data['description']
 
 // Job rotation defaults
