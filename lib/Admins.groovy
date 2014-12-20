f3c6a9ae2851d77938a18ff37b5437b4a25ba421
Pedro Algarvio <pedro@algarvio.me>
2014-12-20 06:22:00 +0000

    As a class!?

--- lib/Admins.groovy
+++ lib/Admins.groovy
@@ -1,19 +1,21 @@
 package lib
 
-List<String> usernames = [
-    'thatch45',
-    's0undt3ch',
-    'UtahDave',
-    'SmithSamuelM',
-    'basepi',
-    'techhat',
-    'whiteinge',
-    'cro',
-    'cachedout',
-    'pass-by-value',
-    'salt-jenkins',
-    'terminalmage',
-    'rallytime',
-    'jfindlay',
-    'pitatus'
-]
+class Admins {
+    static usernames = [
+        'thatch45',
+        's0undt3ch',
+        'UtahDave',
+        'SmithSamuelM',
+        'basepi',
+        'techhat',
+        'whiteinge',
+        'cro',
+        'cachedout',
+        'pass-by-value',
+        'salt-jenkins',
+        'terminalmage',
+        'rallytime',
+        'jfindlay',
+        'pitatus'
+    ]
+}
