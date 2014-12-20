f412384ef155fa630fd728eca92b54b79bc7290c
Pedro Algarvio <pedro@algarvio.me>
2014-12-20 03:18:51 +0000

    The state call is done in the setup virtualenv script

--- scripts/decompress-workspace.sh
+++ scripts/decompress-workspace.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-# Wait for a running salt-call state.sls
-if [ "$(pgrep -f 'salt-call state.sls')" != "" ]; then
-    stdbuf -i0 -o0 -e0 printf "%s" "Waiting on running salt-call to finish "
-    while [ "$(pgrep -f 'salt-call state.sls')" != "" ]; do
-        sleep 1
-        stdbuf -i0 -o0 -e0 printf "%s" "."
-    done
-    stdbuf -i0 -o0 -e0 printf "%s\n\n" "Done!"
-fi
-
-salt-call state.sls projects.clone
 xz -cdk workspace.cpio.xz | cpio -div
