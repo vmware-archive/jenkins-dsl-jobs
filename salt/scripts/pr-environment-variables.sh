3288ab509dd47ee486960a228acf03281d6268b3
Pedro Algarvio <pedro@algarvio.me>
2014-12-23 02:01:39 +0000

    We need to activate the virtual environment

--- salt/scripts/pr-environment-variables.sh
+++ salt/scripts/pr-environment-variables.sh
@@ -1,1 +1,3 @@
+. /srv/virtualenvs/${VIRTUALENV_NAME}/bin/activate > /dev/null 2>&1
+
 salt-jenkins-build --vm-source ${BUILD_VM_NAME} --workspace=${WORKSPACE} --pull-request=${PR} --output-columns=160 --echo-parseable-output
