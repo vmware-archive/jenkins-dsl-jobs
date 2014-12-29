54990b3120d873424664749fb9f34c4c2684a272
Pedro Algarvio <pedro@algarvio.me>
2014-12-29 03:54:10 +0000

    !

--- templates/branches-envvars-commit-status.groovy
+++ templates/branches-envvars-commit-status.groovy
@@ -53,5 +53,5 @@ return [
     VIRTUALENV_SETUP_STATE_NAME: '$virtualenv_setup_state_name',
     BRANCH_NAME: '$branch_name',<% if (build_vm_name != null) { %>
     BUILD_VM_NAME: '$build_vm_name',<% } %><% if (vm_name_nodots != null) { %>
-    JENKINS_VM_NAME: build_env_vars['JENKINS_VM_NAME_PREFIX'] + '_$vm_name_nodots_' + build_number<% } %>
+    JENKINS_VM_NAME: build_env_vars['JENKINS_VM_NAME_PREFIX'] + '_' + '$vm_name_nodots' + '_' + build_number<% } %>
 ]
