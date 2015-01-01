02b0e95d4515c96e23ee740ea061b63c9d28c710
Pedro Algarvio <pedro@algarvio.me>
2015-01-01 09:37:09 +0000

    Make this an importable package

---
+++ src/main/groovy/com/saltstack/jenkins/PullRequestAdmins.groovy
@@ -0,0 +1,21 @@
+package com.saltstack.jenkins
+
+class PullRequestAdmins {
+    static usernames = [
+        'thatch45',
+        's0undt3ch',
+        'UtahDave',
+        'SmithSamuelM',
+        'basepi',
+        'techhat',
+        'whiteinge',
+        'cro',
+        'cachedout',
+        'pass-by-value',
+        'salt-jenkins',
+        'terminalmage',
+        'rallytime',
+        'jfindlay',
+        'pitatus'
+    ]
+}
