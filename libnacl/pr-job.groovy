6dc007cc0ab480dfc569b60fa5aa665287b2b917
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 14:07:09 +0000

    Let's resort to write to a file again

--- libnacl/pr-job.groovy
+++ libnacl/pr-job.groovy
@@ -483,12 +483,14 @@ pr_data.each { pr ->
         }
 }
 
-new_prs.each { pr_id ->
-    pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob("${pr_id}").getJob('main-build')
-    trigger = job.triggers.iterator().next().value
-    repo = trigger.getRepository()
-    pr = repo.getPullRequest(pr_id)
-    repo.check(pr)
-    pr = repo.pulls.get(pr_id)
-    repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
+
+if ( new_prs.length() > 0 ) {
+    def thr = Thread.currentThread()
+    def build = thr?.executable
+
+    new_prs_file = build.getWorkspace().child('new-prs.txt').withWritter { out ->
+        new_prs.each { pr_id ->
+            out.writeLine("${pr_id}")
+        }
+    }
 }
