aacb161d98e13b637be52e44e829ee157efa711b
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 11:49:18 +0000

    Let's see if calling the methods directly works

--- groovy/inject-submitter-email-address.groovy
+++ groovy/inject-submitter-email-address.groovy
@@ -1,5 +1,5 @@
-@GrabResolver(name='jenkins-dsl-jobs', root='http://saltstack.github.io/jenkins-dsl-jobs/')
-@Grab('com.saltstack:jenkins-dsl-jobs:1.0-SNAPSHOT')
+groovy.grape.Grape.addResolver(name='jenkins-dsl-jobs', root='http://saltstack.github.io/jenkins-dsl-jobs/')
+groovy.grape.Grape.grab([group:'com.saltstack', module:'jenkins-dsl-jobs', version:'1.0-SNAPSHOT'])
 
 import javax.mail.internet.InternetAddress
 import com.saltstack.jenkins.EmailNotifications
