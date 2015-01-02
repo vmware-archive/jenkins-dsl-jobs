bfdc83ed0a8469780e51f8b57cd3fdfa05f26bb9
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 15:00:11 +0000

    Update the post script

--- groovy/trigger-pr-jobs.groovy
+++ groovy/trigger-pr-jobs.groovy
@@ -1,15 +1,12 @@
 import jenkins.model.Jenkins
 
-def thr = Thread.currentThread()
-def build = thr?.executable
-
-new_prs = build.getWorkspace().child('new-prs.txt').readToString().split(/(;|,|\n)/)
+new_prs = manager.build.getWorkspace().child('new-prs.txt').readToString().split(/(;|,|\n)/)
 new_prs.each { pr_id ->
-    pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob("${pr_id}").getJob('main-build')
-    trigger = job.triggers.iterator().next().value
+    pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob(pr_id).getJob('main-build')
+    trigger = pr_job.triggers.iterator().next().value
     repo = trigger.getRepository()
-    pr = repo.getPullRequest(pr_id)
+    pr = repo.getPullRequest(pr_id.toInteger())
     repo.check(pr)
-    pr = repo.pulls.get(pr_id)
+    pr = repo.pulls.get(pr_id.toInteger())
     repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
 }
