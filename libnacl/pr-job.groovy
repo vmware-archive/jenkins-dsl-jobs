dc917c6d0b3bb1448d7293f80efeac948af32ac8
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 13:33:31 +0000

    Fix syntax

--- libnacl/pr-job.groovy
+++ libnacl/pr-job.groovy
@@ -39,7 +39,7 @@ folder {
 pr_data.each { pr ->
     folder {
         name("libnacl/pr/${pr.number}")
-        displayName("PR #${pr.number} - ${pr.title}"
+        displayName("PR #${pr.number} - ${pr.title}")
         description = pr.body
     }
 
