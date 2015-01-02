30c92bd9c2bff803fda3cf1df8d752aee2b9f185
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 15:13:39 +0000

    Protect from null

--- groovy/trigger-pr-jobs.groovy
+++ groovy/trigger-pr-jobs.groovy
@@ -4,14 +4,18 @@ def triggered = []
 new_prs = manager.build.getWorkspace().child('new-prs.txt').readToString().split(/(;|,|\n)/)
 new_prs.each { pr_id ->
     if ( triggered.contains(pr_id) == false) {
-        pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob(pr_id).getJob('main-build')
-        manager.listener.logger.println("Triggering build for ${pr_job.getFullDisplayName()}")
-        trigger = pr_job.triggers.iterator().next().value
-        repo = trigger.getRepository()
-        pr = repo.getPullRequest(pr_id.toInteger())
-        repo.check(pr)
-        pr = repo.pulls.get(pr_id.toInteger())
-        repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
+        try {
+            pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob(pr_id).getJob('main-build')
+            manager.listener.logger.println("Triggering build for ${pr_job.getFullDisplayName()}")
+            trigger = pr_job.triggers.iterator().next().value
+            repo = trigger.getRepository()
+            pr = repo.getPullRequest(pr_id.toInteger())
+            repo.check(pr)
+            pr = repo.pulls.get(pr_id.toInteger())
+            repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
+        } catch(e) {
+            manager.listener.logger.println "Failed to get Job libnacl/pr/${pr_id}/main-build"
+        }
         triggered.add(pr_id)
     }
 }
