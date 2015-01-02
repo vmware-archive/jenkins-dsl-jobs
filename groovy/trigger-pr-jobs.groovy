1a0f20536171badf05a799d4f489cb274aa0e2ae
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 15:09:08 +0000

    Some logging and duplicates handling

--- groovy/trigger-pr-jobs.groovy
+++ groovy/trigger-pr-jobs.groovy
@@ -1,12 +1,17 @@
 import jenkins.model.Jenkins
 
+def triggered = []
 new_prs = manager.build.getWorkspace().child('new-prs.txt').readToString().split(/(;|,|\n)/)
 new_prs.each { pr_id ->
-    pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob(pr_id).getJob('main-build')
-    trigger = pr_job.triggers.iterator().next().value
-    repo = trigger.getRepository()
-    pr = repo.getPullRequest(pr_id.toInteger())
-    repo.check(pr)
-    pr = repo.pulls.get(pr_id.toInteger())
-    repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
+    if ( triggered.contains(pr_id) == false) {
+        pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob(pr_id).getJob('main-build')
+        manager.listener.logger.println("Triggering build for ${pr_job.getFullDisplayName()}")
+        trigger = pr_job.triggers.iterator().next().value
+        repo = trigger.getRepository()
+        pr = repo.getPullRequest(pr_id.toInteger())
+        repo.check(pr)
+        pr = repo.pulls.get(pr_id.toInteger())
+        repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
+        triggered.add(pr_id)
+    }
 }
