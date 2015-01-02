e5bc075d8f14dc1140ddd0f4365de605edf37974
Pedro Algarvio <pedro@algarvio.me>
2015-01-02 14:23:48 +0000

    Write

--- groovy/trigger-pr-jobs.groovy
+++ groovy/trigger-pr-jobs.groovy
@@ -3,15 +3,13 @@ import jenkins.model.Jenkins
 def thr = Thread.currentThread()
 def build = thr?.executable
 
-new_prs_file = build.getWorkspace().child('new-prs.txt')
-if ( new_prs_file.length() > 0 ) {
-    new_prs_file.eachLine { pr_id ->
-        pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob("${pr_id}").getJob('main-build')
-        trigger = job.triggers.iterator().next().value
-        repo = trigger.getRepository()
-        pr = repo.getPullRequest(pr_id)
-        repo.check(pr)
-        pr = repo.pulls.get(pr_id)
-        repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
-    }
+new_prs = build.getWorkspace().child('new-prs.txt').readToString().split(/(;|,|\n)/)
+new_prs.each { pr_id ->
+    pr_job = Jenkins.instance.getJob('libnacl').getJob('pr').getJob("${pr_id}").getJob('main-build')
+    trigger = job.triggers.iterator().next().value
+    repo = trigger.getRepository()
+    pr = repo.getPullRequest(pr_id)
+    repo.check(pr)
+    pr = repo.pulls.get(pr_id)
+    repo.helper.builds.build(pr, pr.author, 'Job Created. Start Initial Build')
 }
