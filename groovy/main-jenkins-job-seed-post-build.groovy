6f402c28ca9e1cc4bd49a27a2f671af99b5d6fcb
Pedro Algarvio <pedro@algarvio.me>
2015-01-04 02:59:59 +0000

    Protect from non existing jobs

--- groovy/main-jenkins-job-seed-post-build.groovy
+++ groovy/main-jenkins-job-seed-post-build.groovy
@@ -6,23 +6,26 @@ import com.cloudbees.jenkins.GitHubRepositoryName
  ['raet', 'saltstack/raet'],
  ['salt', 'saltstack/salt'],
  ['sorbic', 'thatch45/sorbic']].each { project, github_repo ->
-    job = Jenkins.instance.getJob(project).get('pr').get('jenkins-seed')
-    if ( job != null ) {
-        repo = GitHubRepositoryName.create("https://github.com/${github_repo}").resolve().iterator().next()
-        repo.getHooks().each { hook ->
-            if ( hook.getName() == 'web' ) {
-                try {
-                    hook_config = hook.getConfig()
-                    if ( hook_config.url.startsWith(job.getAbsoluteUrl()) ) {
-                        hook.delete()
+    try {
+        job = Jenkins.instance.getJob(project).getJob('pr').getJob('jenkins-seed')
+        if ( job != null ) {
+            repo = GitHubRepositoryName.create("https://github.com/${github_repo}").resolve().iterator().next()
+            repo.getHooks().each { hook ->
+                if ( hook.getName() == 'web' ) {
+                    try {
+                        hook_config = hook.getConfig()
+                        if ( hook_config.url.startsWith(job.getAbsoluteUrl()) ) {
+                            hook.delete()
+                        }
+                    } catch(e) {
                     }
-                } catch(e) {
                 }
             }
+            repo.createWebHook(
+                "${job.getAbsoluteUrl()}?token=${job.getAuthToken().getToken()}".toURL(),
+                [GHEvent.PULL_REQUEST]
+            )
         }
-        repo.createWebHook(
-            "${job.getAbsoluteUrl()}?token=${job.getAuthToken().getToken()}",
-            [GHEvent.PULL_REQUEST]
-        )
+    } catch(job_error) {
     }
 }
