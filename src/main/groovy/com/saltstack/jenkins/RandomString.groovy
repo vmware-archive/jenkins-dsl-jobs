4600ab5cdcdcc4ebddfedf34048db729381687b6
Pedro Algarvio <pedro@algarvio.me>
2015-01-04 01:27:55 +0000

    Add random string generator

---
+++ src/main/groovy/com/saltstack/jenkins/RandomString.groovy
@@ -0,0 +1,14 @@
+
+
+class RandomString {
+
+    static public generate(int size = 16) {
+        return generator((('A'..'Z')+('a'..'z')+('0'..'9')).join(), size)
+    }
+
+    static private generator(String alphabet, int n) {
+        new Random().with {
+            (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+      }
+    }
+}
