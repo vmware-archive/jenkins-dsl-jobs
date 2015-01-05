2b63b2d1e1e89a609641d9a2638b12f3de5a714e
Pedro Algarvio <pedro@algarvio.me>
2015-01-05 05:09:39 +0000

    Get data from env

--- master-build/templates/job-seed-post-build.groovy
+++ master-build/templates/job-seed-post-build.groovy
@@ -2,8 +2,7 @@ import groovy.json.*
 import org.kohsuke.github.GHEvent
 import com.cloudbees.jenkins.GitHubRepositoryName
 
-def projects = $projects
-new JsonSlurper().parseText(projects).each { data ->
+new JsonSlurper().parseText(manager.envVars['GITHUB_JSON_DATA']).each { name, data ->
     if ( data.add_branches_webhook == true ) {
         manager.listener.logger.println 'Setting up branches create/delete webhook for ' + data.display_name + ' ...'
         def github_repo_url = "https://github.com/" + data.github_repo
