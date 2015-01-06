48ad03d5d4f363282d2d640f14fd561a25df57ec
Pedro Algarvio <pedro@algarvio.me>
2015-01-06 04:38:57 +0000

    Some protection

--- groovy/inject-submitter-email-address.groovy
+++ groovy/inject-submitter-email-address.groovy
@@ -1,8 +1,10 @@
 import javax.mail.internet.InternetAddress
 
-opt_out = build.getEnvVars().get("EMAIL_OPT_OUT", "").split(/(;|,|\n)/)
-
-recipients = [new InternetAddress(build.getEnvVars()["ghprbActualCommitAuthorEmail"])]
-// Filter opt-out addresses
-opt_out_filtered = recipients.findAll { addr -> opt_out.contains(addr.toString()) == false }
-msg.setRecipients(javax.mail.Message.RecipientType.TO, opt_out_filtered as javax.mail.Address[])
+opt_out = build.getEnvironment().get("EMAIL_OPT_OUT", "").split(/(;|,|\n)/)
+actual_email_address = build.getEnvironment().get("ghprbActualCommitAuthorEmail", null)
+if ( actual_email_address != null ) {
+    recipients = [new InternetAddress(actual_email_address)]
+    // Filter opt-out addresses
+    opt_out_filtered = recipients.findAll { addr -> opt_out.contains(addr.toString()) == false }
+    msg.setRecipients(javax.mail.Message.RecipientType.TO, opt_out_filtered as javax.mail.Address[])
+}
