062fc93ecd21511dbfd76d23646712a230cb4390
Pedro Algarvio <pedro@algarvio.me>
2015-01-06 03:23:05 +0000

    Remove debug echo

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -13,6 +13,5 @@ if [ "$(pgrep -f 'salt-call state.sls')" != "" ]; then
     printf "%s\n\n" "Done!"
 fi
 
-echo "Running command: 'salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar=\"{virtualenv_name: ${VIRTUALENV_NAME}, system_site_packages: ${SYSTEM_SITE_PACKAGES:-false}}\"'"
 salt-call state.sls ${VIRTUALENV_SETUP_STATE_NAME} pillar="{virtualenv_name: ${VIRTUALENV_NAME}, system_site_packages: ${SYSTEM_SITE_PACKAGES:-false}}"
 echo '<<<<<<<<<<<<<< Prepare Virtualenv <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
