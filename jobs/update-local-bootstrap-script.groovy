50db48dcde6a3242212de6869697fe0080cd81eb
Pedro Algarvio <pedro@algarvio.me>
2015-01-09 20:28:52 +0000

    Add a job to update the master's bootstrap script

---
+++ jobs/update-local-bootstrap-script.groovy
@@ -0,0 +1,34 @@
+// Update the bootstrap script
+@GrabResolver(name='jenkins-dsl-jobs', root='http://saltstack.github.io/jenkins-dsl-jobs/')
+@Grab('com.saltstack:jenkins-dsl-jobs:1.2-SNAPSHOT')
+
+import com.saltstack.jenkins.JenkinsPerms
+
+job {
+    name = 'update-bootstrap'
+    displayName('Update Bootstrap Script')
+    description('Update the local copy of the bootstrap script')
+
+    concurrentBuild(allowConcurrentBuild = false)
+
+    // Run in on the master
+    label('master')
+
+    parameters {
+        choiceParam('BRANCH', ['develop', 'stable']),
+        choiceParam('REPOSITORY', ['saltstack', 's0undt3ch'])
+    }
+
+    authorization {
+        JenkinsPerms.usernames.each { username ->
+            JenkinsPerms.permissions.each { permname ->
+                permission("${permname}:${username}")
+            }
+        }
+    }
+
+    // Job Steps
+    steps {
+        shell('wget -O /etc/salt/cloud.deploy.d/bootstrap-salt.sh https://raw.github.com/${REPOSITORY}/salt-bootstrap/${BRANCH}/bootstrap-salt.sh')
+    }
+}
