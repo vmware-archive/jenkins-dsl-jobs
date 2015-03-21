fb8aad21f9b58a4490c78fdf29f8e96dbd57eded
Pedro Algarvio <pedro@algarvio.me>
2015-03-21 19:55:52 +0000

    Update the pr job to use the project class

--- bootstrap/groovy/pr-dsl-job.groovy
+++ bootstrap/groovy/pr-dsl-job.groovy
@@ -3,19 +3,16 @@ import groovy.json.*
 import groovy.text.*
 import jenkins.model.Jenkins
 import com.saltstack.jenkins.PullRequestAdmins
+import com.saltstack.jenkins.projects.Bootstrap
 
 // get current thread / Executor
 def thr = Thread.currentThread()
+
 // get current build
 def build = thr?.executable
 
-def slurper = new groovy.json.JsonSlurper()
-
-def github_json_data = slurper.parseText(build.getEnvironment().get('GITHUB_JSON_DATA', '{}'))
-
 // Common variable Definitions
-def github_repo = 'saltstack/bootstrap'
-def project_description = github_json_data['project_description']
+def project = new Bootstrap()
 
 // Job rotation defaults
 def default_days_to_keep = 90
@@ -32,26 +29,23 @@ def new_prs = [:]
 def template_engine = new SimpleTemplateEngine()
 
 // Define the folder structure
-folder {
-    name('bootstrap')
-    displayName(github_json_data['bootstrap']['display_name'])
-    description = project_description
+folder(project.name) {
+    displayName(project.display_name)
+    description = project.getRepositoryDescription()
 }
-folder {
-    name('bootstrap/pr')
+folder("${project.name}/pr") {
     displayName('Pull Requests')
-    description = project_description
+    description = project.getRepositoryDescription()
 }
 
-github_json_data['open_prs'].each { pr ->
-    folder {
-        name("bootstrap/pr/${pr.number}")
+projects.getOpenPullRequests().each() { pr ->
+    folder("${project.name}/pr/${pr.number}") {
         displayName("PR #${pr.number}")
-        description = "${pr.title}\n\n${pr.body}"
+        description = "<h1>${pr.title}</h1><br/>${pr.body}"
     }
 
     try {
-        existing_job = Jenkins.instance.getJob('bootstrap').getJob('pr').getJob("${pr.number}").getJob('main-build')
+        existing_job = Jenkins.instance.getJob(project.name).getJob('pr').getJob("${pr.number}").getJob('main-build')
         if ( existing_job == null ) {
             new_prs[pr.number] = pr.sha
         }
@@ -61,10 +55,9 @@ github_json_data['open_prs'].each { pr ->
     }
 
     // PR Main Job
-    job(type: BuildFlow) {
-        name = "bootstrap/pr/${pr.number}/main-build"
+    buildFlowJob("${project.name}/pr/${pr.number}/main-build") {
         displayName("Main Build")
-        description("${pr.title}\n\n${pr.body}")
+        description("<h1>${pr.title}</h1><br/>${pr.body}")
         label('worker')
         concurrentBuild(allowConcurrentBuild = false)
 
@@ -73,12 +66,12 @@ github_json_data['open_prs'].each { pr ->
             job_publishers = it.get('publishers').get(0)
             job_publishers.appendNode(
                 'org.zeroturnaround.jenkins.flowbuildtestaggregator.FlowTestAggregator',
-                [plugin: 'build-flow-test-aggregator@1.1-SNAPSHOT']
+                [plugin: 'build-flow-test-aggregator@latest']
             )
             job_properties = it.get('properties').get(0)
             github_project_property = job_properties.appendNode(
                 'com.coravy.hudson.plugins.github.GithubProjectProperty')
-            github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
+            github_project_property.appendNode('projectUrl').setValue("https://github.com/${project.repo}")
             slack_notifications = job_properties.appendNode(
                 'jenkins.plugins.slack.SlackNotifier_-SlackJobProperty')
             slack_notifications.appendNode('room').setValue('#jenkins')
@@ -90,7 +83,7 @@ github_json_data['open_prs'].each { pr ->
             slack_notifications.appendNode('notifyBackToNormal').setValue(true)
             job_publishers.appendNode(
                 'jenkins.plugins.slack.SlackNotifier',
-                [plugin: 'slack@1.2']
+                [plugin: 'slack@latest']
             )
         }
 
@@ -134,14 +127,14 @@ github_json_data['open_prs'].each { pr ->
         }
 
         template_context = [
-            project: 'bootstrap',
+            project: project.name,
             pr_number: pr.number
         ]
         script_template = template_engine.createTemplate(
             readFileFromWorkspace('maintenance/jenkins-seed', 'bootstrap/templates/pr-main-build-flow.groovy')
         )
         rendered_script_template = script_template.make(template_context.withDefault{ null })
-        buildFlowJob(
+        buildFlow(
             rendered_script_template.toString()
         )
 
@@ -181,12 +174,11 @@ github_json_data['open_prs'].each { pr ->
     }
 
     // PR Clone Job
-    def pr_clone_job = job {
-        name = "bootstrap/pr/${pr.number}/clone"
+    freeStyleJob("${project.name}/pr/${pr.number}/clone") {
         displayName("Clone Repository")
 
         concurrentBuild(allowConcurrentBuild = true)
-        description("${pr.title}\n\n${pr.body}")
+        description("<h1>${pr.title}</h1><br/>${pr.body}")
         label('worker')
 
         parameters {
@@ -198,10 +190,10 @@ github_json_data['open_prs'].each { pr ->
             job_properties.appendNode(
                 'hudson.plugins.copyartifact.CopyArtifactPermissionProperty').appendNode(
                     'projectNameList').appendNode(
-                        'string').setValue("bootstrap/pr/${pr.number}/*")
+                        'string').setValue("${project.name}/pr/${pr.number}/*")
             github_project_property = job_properties.appendNode(
                 'com.coravy.hudson.plugins.github.GithubProjectProperty')
-            github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
+            github_project_property.appendNode('projectUrl').setValue("https://github.com/${project.repo}")
         }
 
         wrappers {
@@ -226,14 +218,10 @@ github_json_data['open_prs'].each { pr ->
         }
 
         // Delete old jobs
-        /* Since we're just cloning the repository in order to make it an artifact to
-         * user as workspace for all other jobs, we only need to keep the artifact for
-         * a couple of minutes. Since one day is the minimum....
-         */
         logRotator(
             default_days_to_keep,
             default_nr_of_jobs_to_keep,
-            1,  //default_artifact_days_to_keep,
+            default_artifact_days_to_keep,
             default_artifact_nr_of_jobs_to_keep
         )
 
@@ -241,7 +229,7 @@ github_json_data['open_prs'].each { pr ->
         scm {
             git {
                 remote {
-                    github(github_repo, protocol='https')
+                    github(project.repo, protocol='https')
                     refspec('+refs/pull/*:refs/remotes/origin/pr/*')
                 }
                 branch("origin/pr/${pr.number}/merge")
@@ -258,7 +246,7 @@ github_json_data['open_prs'].each { pr ->
 
         template_context = [
             commit_status_context: 'ci/clone',
-            github_repo: github_repo,
+            github_repo: project.repo,
         ]
         script_template = template_engine.createTemplate(
             readFileFromWorkspace('maintenance/jenkins-seed', 'templates/branches-envvars-commit-status.groovy')
@@ -287,11 +275,10 @@ github_json_data['open_prs'].each { pr ->
     }
 
     // PR Lint Job
-    job {
-        name = "bootstrap/pr/${pr.number}/lint"
+    freeStyleJob("${project.name}/pr/${pr.number}/lint") {
         displayName("Lint")
         concurrentBuild(allowConcurrentBuild = true)
-        description("${pr.title}\n\n${pr.body}")
+        description("<h1>${pr.title}</h1><br/>${pr.body}")
         label('container')
 
         // Parameters Definition
@@ -304,7 +291,7 @@ github_json_data['open_prs'].each { pr ->
             job_properties = it.get('properties').get(0)
             github_project_property = job_properties.appendNode(
                 'com.coravy.hudson.plugins.github.GithubProjectProperty')
-            github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
+            github_project_property.appendNode('projectUrl').setValue("https://github.com/${project.repo}")
         }
 
         wrappers {
@@ -341,7 +328,7 @@ github_json_data['open_prs'].each { pr ->
 
         template_context = [
             commit_status_context: 'ci/lint',
-            github_repo: github_repo,
+            github_repo: project.repo,
             virtualenv_setup_state_name: 'projects.bootstrap.lint'
         ]
         script_template = template_engine.createTemplate(
@@ -356,7 +343,7 @@ github_json_data['open_prs'].each { pr ->
         // Job Steps
         steps {
             // Copy the workspace artifact
-            copyArtifacts("bootstrap/pr/${pr.number}/clone", 'workspace.cpio.xz') {
+            copyArtifacts("${project.name}/pr/${pr.number}/clone", 'workspace.cpio.xz') {
                 buildNumber('${CLONE_BUILD_ID}')
             }
             shell(readFileFromWorkspace('maintenance/jenkins-seed', 'scripts/decompress-workspace.sh'))
