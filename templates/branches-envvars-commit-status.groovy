336b90ae6d0baae994ca8aaf39180052c8e387a1
Pedro Algarvio <pedro@algarvio.me>
2015-03-23 12:59:09 +0000

    `GITHUB_REPO` envvar is not used anymore

--- templates/branches-envvars-commit-status.groovy
+++ templates/branches-envvars-commit-status.groovy
@@ -12,8 +12,6 @@ def build_number = build_env_vars['BUILD_NUMBER'].padLeft(4, '0')
 return [<%
     if ( sudo_salt_call != null ) { %>
     SUDO_SALT_CALL_REQUIRED: '1',<% } %><%
-    if ( github_repo != null ) { %>
-    GITHUB_REPO: '$github_repo',<% } %><%
     if ( virtualenv_name != null ) { %>
     VIRTUALENV_NAME: '$virtualenv_name',<% } %><%
     if ( virtualenv_setup_state_name != null ) { %>
