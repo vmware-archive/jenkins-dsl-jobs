ac02120e73d36979a0def1f7c96aeff1751e37f6
Pedro Algarvio <pedro@algarvio.me>
2015-03-28 15:26:28 +0000

    Let's remove the verboseness of the commands

--- scripts/compress-workspace.sh
+++ scripts/compress-workspace.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo '>>>>>>>>>>>>>> Compress Workspace >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-find . -not -name workspace.cpio.xz | cpio -ov | xz > workspace.cpio.xz
+find . -not -name workspace.cpio.xz | cpio -o | xz > workspace.cpio.xz
 echo '<<<<<<<<<<<<<< Compress Workspace <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
--- scripts/decompress-workspace.sh
+++ scripts/decompress-workspace.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo '>>>>>>>>>>>>>> Decompress Workspace >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-xz -cdk workspace.cpio.xz | cpio -div
+xz -cdk workspace.cpio.xz | cpio -di
 echo '<<<<<<<<<<<<<< Decompress Workspace <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
