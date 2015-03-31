79c0a72ac1a664d564a8df9a1a5524e0814eb715
Pedro Algarvio <pedro@algarvio.me>
2015-03-31 08:03:24 +0100

    Absolute path

--- salt/scripts/download-remote-files.sh
+++ salt/scripts/download-remote-files.sh
@@ -2,7 +2,7 @@
 
 salt-jenkins-build \
   --output-columns=160 \
-  --download-artifact tmp/xml-unittests-output artifacts \
+  --download-artifact /tmp/xml-unittests-output artifacts \
   --download-artifact /tmp/coverage.xml artifacts/coverage \
   --download-artifact /var/log/salt/minion artifacts/logs \
   --download-artifact /tmp/salt-runtests.log artifacts/logs ${JENKINS_SALTCLOUD_VM_NAME}
