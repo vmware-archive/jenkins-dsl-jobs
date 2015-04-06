1dad43104e097795d72567d5b3af216df12cdd94
Pedro Algarvio <pedro@algarvio.me>
2015-04-06 15:13:42 +0100

    Minor cosmetic changes

--- scripts/prepare-virtualenv.sh
+++ scripts/prepare-virtualenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo '<<<<<<<<<<<<<< Prepare Build Env <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+echo '>>>>>>>>>>>>>> Prepare Build Env >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 # Don't call 'salt-call' at the same time
 sleep $(($RANDOM % 12))
 
@@ -13,17 +13,17 @@ fi
 if [ "${VIRTUALENV_NAME}" != "" ]; then
     ${SUDO} salt-call --force-color \
         --retcode-passthrough \
-        --log-file=${WORKSPACE:-.}/salt-call.log \
+        --log-file="${WORKSPACE:-.}/salt-call.log" \
         --log-file-level=trace \
-        state.sls ${VIRTUALENV_SETUP_STATE_NAME} \
+        state.sls "${VIRTUALENV_SETUP_STATE_NAME}" \
         queue=true \
         pillar="{virtualenv_name: ${VIRTUALENV_NAME}, system_site_packages: ${SYSTEM_SITE_PACKAGES:-false}}"
 else
     ${SUDO} salt-call --force-color \
         --retcode-passthrough \
-        --log-file=${WORKSPACE:-.}/salt-call.log \
+        --log-file="${WORKSPACE:-.}/salt-call.log" \
         --log-file-level=trace \
-        state.sls ${VIRTUALENV_SETUP_STATE_NAME} \
+        state.sls "${VIRTUALENV_SETUP_STATE_NAME}" \
         queue=true
 fi
-echo '<<<<<<<<<<<<<< Prepare Build Env <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+echo '<<<<<<<<<<<<<< Prepare Build Env <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
