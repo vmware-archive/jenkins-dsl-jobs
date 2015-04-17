cced39ce004c3d35d7317fcb96712ffe21e5b576
Pedro Algarvio <pedro@algarvio.me>
2015-04-17 10:50:21 +0100

    Revert "Fix variable reference"
    This reverts commit df4a47d5e8e083b2e87bfdb0b0d397c33c3c7304.

--- jobs/bootstrap.groovy
+++ jobs/bootstrap.groovy
@@ -138,8 +138,8 @@ project.branches.each { job_branch ->
         if ( ! data.containsKey(project.name) ) {
             data[project.name] = []
         }
-        if ( ! data[project.name].contains(build_flow_job.name) ) {
-            data[project.name].add(build_flow_job.name)
+        if ( ! data[project_name].contains(build_flow_job.name) ) {
+            data[project_name].add(build_flow_job.name)
         }
         cachefile.write(new JsonBuilder(data).toString(), 'UTF-8')
     }
