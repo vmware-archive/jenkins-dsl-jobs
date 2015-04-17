947f746cb3058fb9f18a81f0658967549d071857
Pedro Algarvio <pedro@algarvio.me>
2015-04-18 00:04:36 +0100

    Each project now gets it's own seed job

--- master-build/master-job.groovy
+++ master-build/master-job.groovy
@@ -1,6 +1,5 @@
 import groovy.json.*
 import groovy.text.*
-import org.apache.commons.lang.RandomStringUtils
 
 // get current thread / Executor
 def thr = Thread.currentThread()
@@ -17,7 +16,9 @@ def default_artifact_days_to_keep = default_days_to_keep
 def default_artifact_nr_of_jobs_to_keep = default_nr_of_jobs_to_keep
 
 def template_engine = new SimpleTemplateEngine()
+def projects = new JsonSlurper().parseText(build.getEnvironment().SEED_PROJECTS)
 def jenkins_perms = new JsonSlurper().parseText(build.getEnvironment().JENKINS_PERMS)
+
 
 folder('maintenance') {
     displayName('Jenkins Maintenance Jobs')
@@ -34,17 +35,18 @@ folder('maintenance') {
 }
 
 
+downstream_builds = []
+projects.keySet().each { project_name ->
+  downstream_builds.add("maintenance/jenkins-${project_name}-seed")
+}
+
 freeStyleJob('maintenance/jenkins-seed') {
     displayName('Jenkins Jobs Seed')
 
-    concurrentBuild(allowConcurrentBuild = true)
+    concurrentBuild(allowConcurrentBuild = false)
 
     configure {
         job_properties = it.get('properties').get(0)
-        job_properties.appendNode('authToken').setValue(new RandomStringUtils().randomAlphanumeric(16))
-        github_project_property = job_properties.appendNode(
-            'com.coravy.hudson.plugins.github.GithubProjectProperty')
-        github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
         auth_matrix = job_properties.appendNode('hudson.security.AuthorizationMatrixProperty')
         auth_matrix.appendNode('blocksInheritance').setValue(true)
     }
@@ -57,14 +59,6 @@ freeStyleJob('maintenance/jenkins-seed') {
                 permission("${permname}:${username}")
             }
         }
-    }
-
-    wrappers {
-        // Add timestamps to console log
-        timestamps()
-
-        // Color Support to console log
-        colorizeOutput('xterm')
     }
 
     // Delete old jobs
@@ -84,16 +78,28 @@ freeStyleJob('maintenance/jenkins-seed') {
     }
     checkoutRetryCount(3)
 
+    wrappers {
+        // Add timestamps to console log
+        timestamps()
+
+        // Color Support to console log
+        colorizeOutput('xterm')
+    }
+
+    // Delete old jobs
+    logRotator(
+        default_days_to_keep,
+        default_nr_of_jobs_to_keep,
+        default_artifact_days_to_keep,
+        default_artifact_nr_of_jobs_to_keep
+    )
+
     environmentVariables {
         groovy('''
-        import com.saltstack.jenkins.Projects
         import com.saltstack.jenkins.JenkinsPerms
-        import com.saltstack.jenkins.PullRequestAdmins
 
         return [
-            SEED_PROJECTS: Projects.toJSON(include_branches = true, include_prs = false),
             JENKINS_PERMS: JenkinsPerms.toJSON(),
-            PULL_REQUEST_ADMINS: PullRequestAdmins.toJSON()
         ]
         '''.stripIndent().trim())
     }
@@ -102,92 +108,93 @@ freeStyleJob('maintenance/jenkins-seed') {
     steps {
         dsl {
             removeAction('DELETE')
-            external('jobs/*.groovy')
+            external('maintenance/*.groovy')
         }
     }
 
     publishers {
-        groovyPostBuild(
-            readFileFromWorkspace('maintenance/jenkins-master-seed', 'master-build/templates/job-seed-post-build.groovy')
-        )
+        downstream(downstream_builds.join(", "), 'FAILURE')
     }
 }
 
-freeStyleJob('maintenance/jenkins-salt-seed') {
-    displayName('Jenkins Salt Branches Seed')
 
-    concurrentBuild(allowConcurrentBuild = true)
+projects.each { project_name, project ->
 
-    configure {
-        job_properties = it.get('properties').get(0)
-        job_properties.appendNode('authToken').setValue(new RandomStringUtils().randomAlphanumeric(16))
-        github_project_property = job_properties.appendNode(
-            'com.coravy.hudson.plugins.github.GithubProjectProperty')
-        github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
-        auth_matrix = job_properties.appendNode('hudson.security.AuthorizationMatrixProperty')
-        auth_matrix.appendNode('blocksInheritance').setValue(true)
-    }
+    freeStyleJob("maintenance/jenkins-${project.name}-seed') {
+        displayName("Jenkins ${project.display_name} Seed')
 
-    authorization {
-        for ( username in jenkins_perms.usernames ) {
-            println "Processing username ${username}"
-            for ( permname in jenkins_perms.project ) {
-                println "Processing project permission ${permname}"
-                permission("${permname}:${username}")
+        concurrentBuild(allowConcurrentBuild = false)
+
+        configure {
+            job_properties = it.get('properties').get(0)
+            github_project_property = job_properties.appendNode(
+                'com.coravy.hudson.plugins.github.GithubProjectProperty')
+            github_project_property.appendNode('projectUrl').setValue("https://github.com/${github_repo}")
+            auth_matrix = job_properties.appendNode('hudson.security.AuthorizationMatrixProperty')
+            auth_matrix.appendNode('blocksInheritance').setValue(true)
+        }
+
+        authorization {
+            for ( username in jenkins_perms.usernames ) {
+                println "Processing username ${username}"
+                for ( permname in jenkins_perms.project ) {
+                    println "Processing project permission ${permname}"
+                    permission("${permname}:${username}")
+                }
             }
         }
-    }
 
-    wrappers {
-        // Add timestamps to console log
-        timestamps()
+        wrappers {
+            // Add timestamps to console log
+            timestamps()
 
-        // Color Support to console log
-        colorizeOutput('xterm')
-    }
-
-    // Delete old jobs
-    logRotator(
-        default_days_to_keep,
-        default_nr_of_jobs_to_keep,
-        default_artifact_days_to_keep,
-        default_artifact_nr_of_jobs_to_keep
-    )
-
-    scm {
-        github (
-            github_repo,
-            branch = '*/testing',
-            protocol = 'https'
-        )
-    }
-    checkoutRetryCount(3)
-
-    environmentVariables {
-        groovy('''
-        import com.saltstack.jenkins.Projects
-        import com.saltstack.jenkins.JenkinsPerms
-        import com.saltstack.jenkins.PullRequestAdmins
-
-        return [
-            SEED_PROJECTS: Projects.toJSON(include_branches = true, include_prs = false),
-            JENKINS_PERMS: JenkinsPerms.toJSON(),
-            PULL_REQUEST_ADMINS: PullRequestAdmins.toJSON()
-        ]
-        '''.stripIndent().trim())
-    }
-
-    // Job Steps
-    steps {
-        dsl {
-            removeAction('DELETE')
-            external('jobs/salt.groovy')
+            // Color Support to console log
+            colorizeOutput('xterm')
         }
-    }
 
-    publishers {
-        groovyPostBuild(
-            readFileFromWorkspace('maintenance/jenkins-master-seed', 'master-build/templates/job-seed-post-build.groovy')
+        // Delete old jobs
+        logRotator(
+            default_days_to_keep,
+            default_nr_of_jobs_to_keep,
+            default_artifact_days_to_keep,
+            default_artifact_nr_of_jobs_to_keep
         )
+
+        scm {
+            github (
+                github_repo,
+                branch = '*/testing',
+                protocol = 'https'
+            )
+        }
+        checkoutRetryCount(3)
+
+        environmentVariables {
+            groovy('''
+            import com.saltstack.jenkins.Projects
+            import com.saltstack.jenkins.JenkinsPerms
+            import com.saltstack.jenkins.PullRequestAdmins
+
+            return [
+                SEED_PROJECTS: Projects.toJSON(include_branches = true, include_prs = false),
+                JENKINS_PERMS: JenkinsPerms.toJSON(),
+                PULL_REQUEST_ADMINS: PullRequestAdmins.toJSON()
+            ]
+            '''.stripIndent().trim())
+        }
+
+        // Job Steps
+        steps {
+            dsl {
+                removeAction('DELETE')
+                external("projects/${project.name}.groovy')
+            }
+        }
+
+        publishers {
+            groovyPostBuild(
+                readFileFromWorkspace('maintenance/jenkins-master-seed', 'master-build/templates/job-seed-post-build.groovy')
+            )
+        }
     }
 }
