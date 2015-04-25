fd7fb456101a8790574ea536bf466e8b48e989b0
Pedro Algarvio <pedro@algarvio.me>
2015-04-25 11:39:54 +0100

    Lets copy the working template

--- common/templates/pr-main-build-flow.groovy
+++ common/templates/pr-main-build-flow.groovy
@@ -23,11 +23,11 @@ guard {
 } rescue {
 
     // Let's instantiate the build flow toolbox
-    //def toolbox = extension.'build-flow-toolbox'
+    def toolbox = extension.'build-flow-toolbox'
 
-    //local_lint_workspace_copy = build.workspace.child('lint')
-    //local_lint_workspace_copy.mkdirs()
-    //toolbox.copyFiles(lint.workspace, local_lint_workspace_copy)
+    local_lint_workspace_copy = build.workspace.child('lint')
+    local_lint_workspace_copy.mkdirs()
+    toolbox.copyFiles(lint.workspace, local_lint_workspace_copy)
 
     //local_unit_workspace_copy = build.workspace.child('unit')
     //local_unit_workspace_copy.mkdirs()
