b570fa43e3baae2d8ad90d5349f553c08eb0c034
Pedro Algarvio <pedro@algarvio.me>
2015-04-25 15:50:50 +0100

    Fix step name

--- projects/raet/groovy/pr-dsl-job.groovy
+++ projects/raet/groovy/pr-dsl-job.groovy
@@ -128,7 +128,7 @@ project.pull_requests.each() { pr ->
             readFileFromWorkspace('maintenance/jenkins-seed', 'common/templates/pr-main-build-flow.groovy')
         )
         rendered_script_template = script_template.make(template_context.withDefault{ null })
-        buildFlowJob(
+        buildFlow(
             rendered_script_template.toString()
         )
 
