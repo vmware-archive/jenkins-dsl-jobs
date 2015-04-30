e660314d9bf1c3e042060275f0c317e33812974c
Pedro Algarvio <pedro@algarvio.me>
2015-04-30 16:30:07 +0100

    Rackspace does not have openSUSE 13.1 image. Build 13.2 for both providers

--- projects/salt.groovy
+++ projects/salt.groovy
@@ -33,7 +33,6 @@ def salt_build_types = [
         'Debian 7',
         'Fedora 20',
         'Fedora 21',
-        'openSUSE 13.1',
         'openSUSE 13.2',
         'Ubuntu 12.04',
         'Ubuntu 14.04',
