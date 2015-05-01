f6548eaf1148a1185580b276880194f1e5b8d61d
Pedro Algarvio <pedro@algarvio.me>
2015-05-02 00:19:10 +0100

    Specify the state to run for build packages

--- projects/salt/scripts/branches-run-tests.sh
+++ projects/salt/scripts/branches-run-tests.sh
@@ -5,10 +5,11 @@ sleep $(($RANDOM % 10))
 
 salt-jenkins-build \
   --output-columns=160 \
-  --vm-source ${BUILD_VM_SOURCE} \
+  --vm-source=${BUILD_VM_SOURCE} \
   --cloud-deploy \
   --test-prep-sls=projects.salt.${BRANCH_NAME}.unit \
-  --test-git-commit ${GIT_COMMIT} \
-  --bootstrap-salt-commit ${SALT_MINION_BOOTSTRAP_RELEASE} \
+  --test-git-commit=${GIT_COMMIT} \
+  --bootstrap-salt-commit=${SALT_MINION_BOOTSTRAP_RELEASE} \
   --test-default-command \
-  --build-packages
+  --build-packages \
+  --build-packages-sls=projects.salt.buildpackage
